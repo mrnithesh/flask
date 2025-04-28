6970618cbd3cc1c9dd8b31777c2ecce48a7dd7e1
mrnithesh <mr.nithesh.k@gmail.com>
2025-04-28 21:30:22 +0530

    dynamic url

--- practice/app.py
+++ practice/app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+
+@app.route('/user/<name>')
+def greet(name):
+    return f'Hello, {name}!'
+
 if __name__ == '__main__':
     app.run(debug=True)
