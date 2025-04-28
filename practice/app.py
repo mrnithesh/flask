3b324445fe47b0a602085f7d4ecb283f0ea0e085
mrnithesh <mr.nithesh.k@gmail.com>
2025-04-28 21:39:04 +0530

    render html templates

--- practice/app.py
+++ practice/app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
 
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/user/<name>')
 def greet(name):
